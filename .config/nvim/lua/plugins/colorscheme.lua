68d4350f2bf006997d66e510a044e9d3dba1aeaa
brandon-kyle-bailey <brandon.bailey@produce8.com>
2025-08-27 11:31:11 -0300

    bug: fix catppuccin bufferline integration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -4,6 +4,10 @@ return {
     name = "catppuccin",
     lazy = false,
     priority = 1000,
+    opts = function()
+      local bufferline = require("catppuccin.groups.integrations.bufferline")
+      bufferline.get = bufferline.get or bufferline.get_theme
+    end,
     config = function()
       vim.cmd.colorscheme("catppuccin")
     end,
