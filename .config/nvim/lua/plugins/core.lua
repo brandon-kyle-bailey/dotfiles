9f3c4136db120d96bed362d627cb2b1c4abee1cf
brandon-kyle-bailey <brandonkylebailey@outlook.com>
2026-04-25 14:41:31 -0300

    chore: push

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -2,7 +2,9 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "default",
+      colorscheme = "retrobox",
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "default",
     },
   },
 }
